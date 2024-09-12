a2d00d9fca19c7683b84db1f55a63dc86adb5ca4
AndreaGonzalezG <mariaandreagg@gmail.com>
2024-09-12 11:45:08 +0200

    desarrollo de ejercicios 10-17

--- Solucion_evaluacion_mod2.sql
+++ Solucion_evaluacion_mod2.sql
@@ -71,29 +71,69 @@ GROUP BY rating;
 /* 10. Encuentra la cantidad total de películas alquiladas por cada cliente y muestra el ID del cliente, 
 su nombre y apellido junto con la cantidad de películas alquiladas.*/
 
+SELECT r.customer_id, c.first_name, c.last_name, COUNT(r.rental_id) AS TotalPeliculasAlquiladas
+FROM rental AS r
+INNER JOIN customer AS c
+ON c.customer_id = r.customer_id
+GROUP BY r.customer_id;
+
+/* 11. Encuentra la cantidad total de películas alquiladas por categoría y muestra el nombre de la categoría junto con el 
+recuento de alquileres.*/
+
+SELECT c.name AS nombre , COUNT(r.rental_id) AS Total_Alquiladas
+FROM category AS c
+LEFT JOIN film_category AS fc ON c.category_id = fc.category_id
+LEFT JOIN inventory AS i ON i.film_id = fc.film_id
+LEFT JOIN rental AS r ON i.inventory_id= r.inventory_id
+GROUP BY nombre;
+
+/* 12. Encuentra el promedio de duración de las películas para cada clasificación de la tabla film y muestra la
+ clasificación junto con el promedio de duración. */
+
+SELECT rating, AVG(length) AS Promedio_duracion
+FROM film
+GROUP BY rating;
 
 
+-- 13. Encuentra el nombre y apellido de los actores que aparecen en la película con title "Indian Love".
+
+SELECT a.first_name, a.last_name
+FROM actor AS a
+INNER JOIN film_actor AS fa ON  fa.actor_id = a.actor_id
+INNER JOIN film AS f ON f.film_id = fa.film_id
+WHERE f.title = "Indian Love" AND f.film_id = 458;
 
 
+-- 14. Muestra el título de todas las películas que contengan la palabra "dog" o "cat" en su descripción.
+
+SELECT title, description
+FROM film 
+WHERE description REGEXP '(dog|cat)';
+
+-- 15. Hay algún actor o actriz que no apareca en ninguna película en la tabla film_actor.
+
+SELECT a.actor_id, a.first_name, a.last_name
+FROM actor AS a
+WHERE NOT EXISTS ( SELECT fa.actor_id
+					FROM film_actor AS fa 
+);
 
 
+-- 16. Encuentra el título de todas las películas que fueron lanzadas entre el año 2005 y 2010.
 
+SELECT title, release_year
+FROM film 
+WHERE release_year BETWEEN 2005 AND 2010;
 
+-- 17. Encuentra el título de todas las películas que son de la misma categoría que "Family".
 
+SELECT c.name AS nombre_categoria , title
+FROM category AS c
+INNER JOIN film_category AS fc ON fc.category_id = c.category_id
+INNER JOIN film AS f ON f.film_id = fc.film_id
+WHERE c.name = "Family";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- 18. Muestra el nombre y apellido de los actores que aparecen en más de 10 películas.
 
 
 
